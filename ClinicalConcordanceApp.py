373290610514ded21396867a13df02f6d32d464e
dgavlock <dgavlock@gmail.com>
2022-05-10 14:40:44 -0400

    added AUC ROC metric

--- ClinicalConcordanceApp.py
+++ ClinicalConcordanceApp.py
@@ -51,7 +51,6 @@ from sklearn.metrics import confusion_matrix
 
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
-
 st.sidebar.write("# Clinical Concordance Options")
 
 st.markdown('''## Microphysiology Systems Database Clinical Concordance Application''')
@@ -155,9 +154,7 @@ if file1 != None:
     y_probas = best_model.predict_proba(X_test)
     labels=y_pred
 
-    roc_auc_score(y_test, y_probas, multi_class='ovo')
-
-    roc_auc_score(train_y, best_model.predict_proba(train_X), average='macro', multi_class='ovo')
+    st.column(st.metric("Model Accuracy:", '{0:.4g}%'.format(accuracy_score(best_model.predict(X_test), y_test)*100) ), st.metric("ROC AUC Score:", '{0:.4g}'.format(roc_auc_score(y_test, y_probas, multi_class='ovo'))))
 
     fig, ax = plt.subplots()
     sns.heatmap(confusion_matrix(y_test, best_model.predict(X_test)), annot=True, xticklabels=y_labels, yticklabels=y_labels)
